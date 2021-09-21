b9b5e7d676f409243d4ebdf3fedd5a91e1afda42
Sergey Shanshin <sergey.shanshin@jetbrains.com>
2021-09-21 17:57:58 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.5.255-SNAPSHOT"
+    id("kotlinx-kover") version "0.2.0"
+}
+
+group = "com.jetbrains"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+
+    extensions.configure(kotlinx.kover.KoverTaskExtension::class) {
+        useJacoco = true
+    }
+}
+
+
