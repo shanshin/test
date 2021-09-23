84fb8659927ae1715912fa9a06d7691fc070ec12
Egor Tolstoy <igrekde@gmail.com>
2021-09-23 15:55:43 +0400

    Implemented new service

---
+++ src/main/kotlin/org/jetbrains/kover/xml/Service.kt
@@ -0,0 +1,11 @@
+package org.jetbrains.kover.xml
+
+class Service {
+    fun getSomeData(): String {
+        return "abc"
+    }
+
+    fun deleteSomeData(): Boolean {
+        return true
+    }
+}
